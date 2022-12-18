5385c6c0244e777fe6ad371ca24c992205087948
Flávia <flaviabrazx@gmail.com>
2022-12-18 15:09:44 -0300

    Commit inicial

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+
+
+function Home () {
+    return (
+        <div className="App">
+            <ul className="container">
+                
+
+                <li>
+                    <div className="poster-img">
+                        <img/>
+                        <div className="icone-play"></div>
+                    </div>
+                    <div className="body-movie-description">
+                        <h2> Title </h2>
+                        <span>Type/Drama and pthers </span>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="poster-img">
+                        <img/>
+                        <div className="icone-play"></div>
+                    </div>
+                    <div className="body-movie-description">
+                        <h2> Title </h2>
+                        <span>Type/Drama and pthers </span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Home
